144e8b8b2a48b673dd13ffb09e500f514d6be698
Alex <lieubv@gmail.com>
2020-04-01 09:43:05 +0700

    Initial Commit

---
+++ SlideUpMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideUpMenu
+//
+//  Created by alex on 4/1/20.
+//  Copyright © 2020 alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
